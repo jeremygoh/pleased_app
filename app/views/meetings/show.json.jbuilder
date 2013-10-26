d1f86732b0cf51075bae999a8b53261ed8ec8083
Jonas Teeberg <teeberg@gmx.de>
2013-10-26 10:56:28 -0700

    Created new scaffold for meetings

---
+++ app/views/meetings/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @meeting, :date, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 PleasedApp::Application.routes.draw do
 
+  resources :meetings
+
   devise_for :users
   resources :patients
 
