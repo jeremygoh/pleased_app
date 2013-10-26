1dc04ba21b422cd2a2fb8697774f9fc475766406
Jeremy Goh <jg485host@gmail.com>
2013-10-26 12:19:02 -0700

    added responsive head tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20131026190635) do
-
 
   create_table "comments", force: true do |t|
     t.string   "comment"
