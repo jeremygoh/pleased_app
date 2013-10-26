c098f53a732bf0c221fdf4568690d8750e2bbc59
Jeremy Goh <jg485host@gmail.com>
2013-10-26 08:50:49 -0700

    change name of user type column to role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131026152141) do
+ActiveRecord::Schema.define(version: 20131026154618) do
 
   create_table "patients", force: true do |t|
     t.string   "first_name"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20131026152141) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.boolean  "admin"
-    t.integer  "type"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
