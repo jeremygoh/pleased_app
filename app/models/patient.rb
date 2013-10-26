7b83e2a890c78d1accd6dd8f33864ee1b46affdc
Jonas Teeberg <teeberg@gmx.de>
2013-10-26 10:25:50 -0700

    Add group reference to patients table

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,3 @@
 class Patient < ActiveRecord::Base
+	has_one :group
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131026154618) do
+ActiveRecord::Schema.define(version: 20131026171758) do
 
   create_table "patients", force: true do |t|
     t.string   "first_name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20131026154618) do
     t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "group_id"
   end
 
   create_table "users", force: true do |t|
