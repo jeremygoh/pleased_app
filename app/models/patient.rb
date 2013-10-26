5bd5b020652e7e28f063d08e8273009c1e01b414
Jeremy Goh <jg485host@gmail.com>
2013-10-26 15:29:45 -0700

    Added patient validation

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,10 @@
 class Patient < ActiveRecord::Base
-	belongs_to :group
+  validates :first_name, presence: true
+	validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+
+  belongs_to :group
 	has_many :comments
 	has_many :attends, :dependent => :delete_all
 	has_many :meetings, through: :attends
