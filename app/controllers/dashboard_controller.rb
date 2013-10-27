ee8ee94ff5be23ac9209aa1b20c3ffeee11ea647
AliciaKoo <aliciakoo_0@hotmail.com>
2013-10-27 00:22:53 -0700

    fixed meeting input for dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
 	def user
 		@past_meetings = Meeting.where(:date => 1.month.ago..Date.today - 1.day)
 
-		@today_meetings = Meeting.where('start BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day)
+		@today_meetings = Meeting.where(Date.today)
 
 		@future_meetings = Meeting.where(:date => Date.today + 1.day..Date.today + 30.day)
 	end
