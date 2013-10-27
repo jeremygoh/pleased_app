3ca701672e28f2765ee60dd53b6e14bcd0d01a70
Jeremy Goh <jg485host@gmail.com>
2013-10-27 03:45:40 -0700

    removed email validations from patient and fixed patient group id save

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,6 @@
 class Patient < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
   validates :phone, presence: true
 
   belongs_to :group
