5c05db0056f25c2800f645eb361ae6d8fa78573b
AliciaKoo <aliciakoo_0@hotmail.com>
2013-10-27 00:01:29 -0700

    little progress in m_dashboard dates

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,11 @@
 class DashboardController < ApplicationController
 
 	def user
+		@past_meetings = Meeting.where(:date => 1.month.ago..Date.today - 1.day)
+
+		@today_meetings = Meeting.where('start BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day)
+
+		@future_meetings = Meeting.where(:date => Date.today + 1.day..Date.today + 30.day)
 	end
 
 	def admin
