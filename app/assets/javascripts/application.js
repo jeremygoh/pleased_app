850d3c741f3d3ffbf545cb3f1fb725a788a15bc7
Jonas Teeberg <teeberg@gmx.de>
2013-10-27 10:22:18 -0700

    Fix JavaScript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,34 +18,7 @@
 
 $(function() {
   updateTime();
-
-  $(".attend_checkbox").on("click", function(event){
-    checkbox = $(this)
-    parent_form = $(this).parent();
-    var valuesToSubmit = $(parent_form).serialize();
-    meeting_id = $(parent_form).children('.meeting').val()
-    url = '/meetings/' + meeting_id + "/attends"
-    $.ajax({
-        type: "POST",
-        url: url,
-        data: valuesToSubmit,
-        success: function(){
-          console.log("sdsdsd");
-          $('body').prepend('<div id="attend-notifications"></div>')
-          $('#attend-notifications').text("Successfully updated attendance")
-          $('#attend-notifications').fadeOut(2000)
-        }
-        }
-      )
-  });
-
-
 });
-
-function pad2(number)
-{
-  return number < 10 ? '0'+number : number;
-}
 
 function updateTime()
 {
@@ -55,12 +28,8 @@ function updateTime()
   var day = date.getDate();
   var year = date.getFullYear();
 
-  var hour = pad2(date.getHours());
-  var minute = pad2(date.getMinutes());
-  var second = pad2(date.getSeconds());
-
-  var str = day+'/'+month+'/'+year;//+', '+hour+':'+minute+':'+second;
-
+  var str = day+'/'+month+'/'+year;
   $('span.current_time').html(str);
-  window.setTimeout(updateTime, 60000);
+
+  window.setTimeout(updateTime, 1000);
 }
