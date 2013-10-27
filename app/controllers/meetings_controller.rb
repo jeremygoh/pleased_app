e8b00c13aaa961938877a728aebc77f6d31d0a76
Jonas Teeberg <teeberg@gmx.de>
2013-10-26 19:56:01 -0700

    Fix meeting interaction

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,64 +1,44 @@
 class MeetingsController < ApplicationController
   before_action :set_meeting, only: [:show, :edit, :update, :destroy]
 
-  # GET /meetings
-  # GET /meetings.json
   def index
     @meetings = Meeting.all
   end
 
-  # GET /meetings/1
-  # GET /meetings/1.json
   def show
   end
 
-  # GET /meetings/new
   def new
     @meeting = Meeting.new
   end
 
-  # GET /meetings/1/edit
   def edit
+    @meeting = Meeting.find(params[:id])
   end
 
-  # POST /meetings
-  # POST /meetings.json
   def create
     @meeting = Meeting.new(meeting_params)
-
-    respond_to do |format|
-      if @meeting.save
-        format.html { redirect_to @meeting, notice: 'Meeting was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @meeting }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @meeting.errors, status: :unprocessable_entity }
-      end
+    if @meeting.save
+      flash[:success] = "Meeting was successfully created"
+      redirect_to @meeting
+    else
+      flash[:error] = @meeting.errors.full_messages[0]
+      redirect_to new_meeting_url
     end
   end
 
-  # PATCH/PUT /meetings/1
-  # PATCH/PUT /meetings/1.json
   def update
-    respond_to do |format|
-      if @meeting.update(meeting_params)
-        format.html { redirect_to @meeting, notice: 'Meeting was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @meeting.errors, status: :unprocessable_entity }
-      end
+    if @meeting.update(meeting_params)
+      redirect_to @meeting
+      flash[:success] = "Meeting was succesfully updated."
+    else
+      flash[:error] = @meeting.errors.full_messages[0]
     end
   end
 
-  # DELETE /meetings/1
-  # DELETE /meetings/1.json
   def destroy
     @meeting.destroy
-    respond_to do |format|
-      format.html { redirect_to meetings_url }
-      format.json { head :no_content }
-    end
+    redirect_to meetings_url
   end
 
   private
